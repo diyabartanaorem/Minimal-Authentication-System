d3965328f2501cccef556f0ff6026e2298b89138
Diyabarta Naorem <naoremdiyabarta@gmail.com>
2024-01-06 22:03:32 +0530

    project setup and basic frontend

---
+++ RUN.md
@@ -0,0 +1,6 @@
+### Steps to Run my project
+
+1. run the command `npm i` to install all the node modules
+2. make sure the database is connected with the correct `URL`.
+
+3. launch the app in the specific port no in your browser
---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const port = 5500;
+
+// middleware
+app.use(express.static("public"));
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURL = "mongodb://localhost:27017";
+mongoose
+  .connect(dbURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) =>
+    app.listen(port, () => {
+      console.log(`App listening on port ${port}`);
+    })
+  )
+  .catch((err) => console.log(err));
+
+// routes
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", (req, res) => res.render("smoothies"));
