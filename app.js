8a10e3259cbcda0e4a449bc694833cddd484eab0
Diyabarta Naorem <naoremdiyabarta@gmail.com>
2024-01-10 18:18:28 +0530

    setting cookie primers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 5500;
@@ -8,6 +9,7 @@ const port = 5500;
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -31,3 +33,23 @@ mongoose
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
+
+// cookies
+app.get('/set-cookies', (req, res)=>{
+  // res.setHeader('Set-Cookie', 'newUser=true');
+
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true} );
+
+  res.send('You got the cookies!');
+});
+
+
+
+app.get('/read-cookies', (req, res)=>{
+
+  const cookies = req.cookies;
+  console.log(cookies);
+
+  res.json(cookies);
+});
