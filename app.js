6d8337fa4bf247517f3e0f304c7c268b4aaddf4b
Diyabarta Naorem <naoremdiyabarta@gmail.com>
2024-01-11 11:46:38 +0530

    making the jwt and updating  frontend for the  signup page

--- app.js
+++ app.js
@@ -34,22 +34,22 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
 app.use(authRoutes);
 
-// cookies
-app.get('/set-cookies', (req, res)=>{
-  // res.setHeader('Set-Cookie', 'newUser=true');
+// // cookies
+// app.get('/set-cookies', (req, res)=>{
+//   // res.setHeader('Set-Cookie', 'newUser=true');
 
-  res.cookie('newUser', false);
-  res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true} );
+//   res.cookie('newUser', false);
+//   res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true} );
 
-  res.send('You got the cookies!');
-});
+//   res.send('You got the cookies!');
+// });
 
 
 
-app.get('/read-cookies', (req, res)=>{
+// app.get('/read-cookies', (req, res)=>{
 
-  const cookies = req.cookies;
-  console.log(cookies);
+//   const cookies = req.cookies;
+//   console.log(cookies);
 
-  res.json(cookies);
-});
+//   res.json(cookies);
+// });
