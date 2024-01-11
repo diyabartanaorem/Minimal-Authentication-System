354266500d5a5bda5fb9cf64ceed7a3df98d3f12
Diyabarta Naorem <naoremdiyabarta@gmail.com>
2024-01-11 23:18:04 +0530

    logging the user in

--- models/User.js
+++ models/User.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema({
         required: [true, "Please enter an email"],
         unique: true,
         lowercase: true,
-        validate: [isEmail, "Please enter a valid email"]
+        validate: [isEmail,  "Please enter a valid email"]
     },
     password: {
         type: String,
@@ -25,7 +25,23 @@ userSchema.pre('save', async function(next){
     const salt = await bcrypt.genSalt();
     this.password = await bcrypt.hash(this.password, salt);
     next();
-})
+});
+
+
+// static model to login user 
+userSchema.statics.login = async function( email, password){
+    const user = await this.findOne({ email });
+    if (user) {
+        const auth = await bcrypt.compare(password, user.password);
+        if (auth) {
+            return user;
+        }
+        throw Error("Incorrect password");
+    }
+    throw Error("Incorrect email");
+}
+
+
 const User = mongoose.model("user", userSchema);
 
 module.exports = User;
